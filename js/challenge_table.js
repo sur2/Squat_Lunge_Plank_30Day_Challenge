50056b6d5c39331664a60aa11383269fe431c144
JEONG PYO YONG <sur2@naver.com>
2020-07-15 23:48:12 +0900

    make table

--- js/challenge_table.js
+++ js/challenge_table.js
@@ -4,22 +4,30 @@ const challengeTable = document.createElement('table');
 challengeTable.className = 'challenge_table';
 challenge.appendChild(challengeTable);
 
-const WORKOUT = {
-    days : 0,
-    squat : 0,
-    lunge : 0,
-    plank : 0,
-};
+
 
 let WORKOUTS = [];
 
 function makeTable() {
-    const tableRow = document.createElement('tr');
-    const tableDate = document.createElement('td');
+    let week = 0;
+    for(let d = 1; d < 31; d++) {
+        if((d - 1) % 6 == 0) {
+            const tableRow = document.createElement('tr');
+            challengeTable.appendChild(tableRow);
+            tableRow.className = `trWeek${++week}`;
+        }
+        const tableDate = document.createElement('td');
+        document.querySelector(`.trWeek${week}`).appendChild(tableDate);
+        tableDate.className = `tdDay${d}`;
+        tableDate.innerHTML = `Day${d}`;
+        const divWorkOut = document.createElement('div');
+        divWorkOut.className = `divWorkOut${d}`;
+        tableDate.appendChild(divWorkOut);
+    }
+}
 
-    tableDate.innerHTML = 'Hi';
-    tableRow.appendChild(tableDate);
-    challengeTable.appendChild(tableRow);
+function loadJSON() {
+    
 }
 
 function init() {
